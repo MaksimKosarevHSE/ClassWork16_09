7d9fa29a2adf02bc6e83c5fd01657d6d7dcba2c8
Max <maksimkosarev96@gmail.com>
2024-09-16 08:36:37 +0300

    File 1 / Added input

--- src/File1.java
+++ src/File1.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class File1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Введите числа:");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println("Спасибо за ввод!");
     }
 }
