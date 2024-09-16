61f0201e4e06a023c488f31d875a8c0483bc3750
Max <maksimkosarev96@gmail.com>
2024-09-16 08:40:10 +0300

    File1 / Added operaiton

--- src/File1.java
+++ src/File1.java
@@ -7,6 +7,6 @@ public class File1 {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
-        System.out.println("Спасибо за ввод!");
+        int result = a + b;
     }
 }
